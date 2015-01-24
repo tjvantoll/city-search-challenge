01341c7ab7008d6a26a718e14627d3a2b686c2d8
TJ VanToll <tj.vantoll@gmail.com>
2015-01-24 11:38:27 -0500

    Initial difficulty configuration

--- app/js/levels.js
+++ app/js/levels.js
@@ -3,37 +3,38 @@
 
 	// The current state of the game
 	var currentLevel = 1,
-		CITIES_PER_LEVEL = 3,
-		NUMBER_OF_LEVELS = 5,
 
 		// Raw data on the levels themselves
 		data = {
-			1: {
-				population: 4000000,
-				km: 200000
-			},
-			2: {
-				population: 3000000,
-				km: 100000
-			},
-			3: {
-				population: 2000000,
-				km: 50000
-			},
-			4: {
-				population: 1000000,
-				km: 50000
-			},
-			5: {
-				population: 500000,
-				km: 50000
-			}
-		};
+			1:  { population: 3000000, km: 10000 },
+			2:  { population: 2500000, km: 9000 },
+			3:  { population: 2000000, km: 8000 },
+			4:  { population: 1500000, km: 7000 },
+			5:  { population: 1000000, km: 6000 },
+
+			// Country borders go away
+			6:  { population: 1500000, km: 6000 },
+			7:  { population: 1250000, km: 5000 },
+			8:  { population: 1000000, km: 4000 },
+			9:  { population: 875000,  km: 3000 },
+			10: { population: 750000,  km: 2000 },
+
+			// Country names go away
+			11: { population: 1250000, km: 5000 },
+			12: { population: 1000000, km: 4000 },
+			13: { population: 800000,  km: 3000 },
+			14: { population: 600000,  km: 2000 },
+			15: { population: 300000,  km: 1000 }
+		},
+
+		// Constants
+		CITIES_PER_LEVEL = 5,
+		NUMBER_OF_LEVELS = data.length;
 
 	Object.keys( data ).forEach(function( key ) {
 		data[ key ].aids = {
-			countryNames: ( key < 3 ),
-			countryBorders: ( key < 5 )
+			countryBorders: ( key <= 5 ),
+			countryNames: ( key <= 10 )
 		};
 	});
 
