2f28e50b848b0d1cdc1a89bf15073a45e36e1d96
TJ VanToll <tj.vantoll@gmail.com>
2015-01-30 15:39:05 -0500

    Apparently Analytics can't read the app's version #?

--- app/js/external/analytics.js
+++ app/js/external/analytics.js
@@ -1,6 +1,6 @@
 (function( g ) {
 	var productId = "aee3e6911acc4709bf01756445fce70d";
-	var version = "1.0"; // Your app version here
+	var version = "3.1.0"; // Your app version here
 
 	// Make analytics available via the window.analytics variable
 	// Start analytics by calling window.analytics.Start()
